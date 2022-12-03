79c5e600abd456635206791ca70cda2eb026e609
Jonathan Clemons <clemons.jonathan@gmail.com>
2022-12-02 20:25:09 -0600

    day01 template alterations

--- day01/part1.py
+++ day01/part1.py
@@ -11,10 +11,21 @@ INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 def compute(s: str) -> int:
-    return max(
-        sum(int(line) for line in part.splitlines())
-        for part in s.split('\n\n')
-    )
+    l = s.split('\n')
+
+    r = []
+    c = 0
+    for i in l:
+        if i == '':
+            r.append(int(c))
+            c = 0
+        else:
+            c += int(i)
+
+    r.sort(reverse=True)
+
+    # TODO: implement solution here!
+    return sum(r[:3])
 
 
 INPUT_S = '''\
@@ -35,11 +46,74 @@ INPUT_S = '''\
 '''
 EXPECTED = 24000
 
+INPUT_2 = '''\
+9548
+3738
+
+18492
+17104
+1738
+'''
+EXPECTED_2 = 37334
+
+INPUT_3 = '''\
+9548
+3738
+
+45000
+
+18492
+17104
+1738
+'''
+EXPECTED_3 = 45000
+
+INPUT_4 = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+
+7000
+8000
+9000
+
+
+10000
+'''
+EXPECTED_4 = 24000
+
+TWIST_1 = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+TW_EXPECTED_1 = 45000
 
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
-        (INPUT_S, EXPECTED),
+        # (INPUT_S, EXPECTED),
+        # (INPUT_2, EXPECTED_2),
+        # (INPUT_3, EXPECTED_3),
+        # (INPUT_4, EXPECTED_4),
+        (TWIST_1, TW_EXPECTED_1),
     ),
 )
 def test(input_s: str, expected: int) -> None:
