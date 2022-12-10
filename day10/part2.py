4c407d8b29fb186d4a12c20accf79d901cf3d5b9
Jonathan Clemons <clemons.jonathan@gmail.com>
2022-12-10 11:03:48 -0600

    day10

---
+++ day10/part2.py
@@ -0,0 +1,233 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def update_crt(cycle: int, sprite_center: int) -> tuple [int, int, str]:
+    row = (cycle - 1) // 40
+    pos = (cycle - 1) - (row * 40)
+
+    pixel = '.'
+    if pos in [sprite_center - 1, sprite_center, sprite_center + 1]:
+        pixel = '#'
+
+    return (row, pos, pixel)
+
+
+def compute(s: str) -> int:
+    sp_ctr = 1
+    cycle = 0
+
+    crt = [['.' for x in range(0,40)] for y in range(0,6)]
+
+    lines = s.splitlines()
+    for line in lines:
+        l = line.split(' ')
+
+        cmd = l[0]
+        if cmd == 'noop':
+            cycle += 1
+
+            r, p, px = update_crt(cycle, sp_ctr)
+            crt[r][p] = px
+
+        elif cmd == 'addx':
+            cycle += 1
+            r, p, px = update_crt(cycle, sp_ctr)
+            crt[r][p] = px
+
+
+            cycle += 1
+            r, p, px = update_crt(cycle, sp_ctr)
+            crt[r][p] = px
+
+            sp_ctr += int(l[1])
+
+
+    for i in crt:
+        print(''.join(i))
+
+    return 0
+
+
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+EXPECTED = 13140
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
